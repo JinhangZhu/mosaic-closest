98b49414f329fd1b239ecfdaa27db02a13b16efc
JinhangZhu <zhujinhangmr@163.com>
2020-09-01 16:33:48 +0800

    Versions truly removed

--- Version5/downsize.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread('mercy_down.jpg')
-img_shape = list(img.shape)
-resized = cv2.resize(img,None,fx=0.5,fy=0.5)
-cv2.imwrite('mercy_down_down.jpg',resized)
-print('done!')
